710ee250516eaafee794c4ac80a5539da70ca75f
NirmeetGandhi <nirmeetgandhi1@gmail.com>
2024-04-01 16:09:38 +0530

    Initial Commit

--- src/components/CSStransition.jsx
+++ src/components/CSStransition.jsx
@@ -13,6 +13,13 @@ const CssTr = () => {
       <CSSTransition
         in={show}
         timeout={5000}
+        // will automatically add this classname before any state updation only
+        // ---------------------------
+        // cssSq-enter-active
+        // cssSq-enter-done
+        // cssSq-exit-active
+        // cssSq-exit-done
+        
         classNames="cssSq"
       >
         <div className="cssSq">
--- src/components/Transition.jsx
+++ src/components/Transition.jsx
@@ -1,41 +1,33 @@
-import { useState } from 'react';
-import { Transition } from 'react-transition-group'
+import { useState } from "react";
+import { Transition } from "react-transition-group";
 
 const TransitionComp = () => {
-  let [show,setShow] = useState(false)
 
+  const [show , setShow] = useState(false);
 
-  const showDiv = () =>{
-    setShow(!show);
-  }
+  return (
+ 
+    <>
+      <Transition
+      in = {show}
+      timeout={2000}
 
-    return(
-      <>
-        <Transition
-          in={show}
-          timeout={{
-            enter:2000,
-            exit:500
-          }}
-          enter={false}
-          exit={false}
-          onEnter={(node)=>{
-            console.log('ENTER')
-          }}
-          onExit={(node)=>{
-            console.log('EXIT')
-          }}
-        >
-          { state => 
-            <div className={`square square-${state}`}>
-              {`square square-${state}`}
-            </div>
-          }
-        </Transition>
-        <hr/>
-        <button className='btn btn-primary' onClick={showDiv}>Show</button>
-      </>
-    )
+      >
+        { state => 
+        // will be 4 states - entering , entered , exiting , exited
+
+        // can be either
+        // square-entering , square-entered , square-exiting , square-exited
+          <div className={`square square-${state}`}>
+            {`square square-${state}`}
+          </div>
+        }
+      </Transition>
+    <hr />
+    <button className="btn btn-primary" onClick={()=> setShow(!show)}Show>Toggle</button>
+    </>
+
+  )
 };
-  
+
 export default TransitionComp;
